2d80688f56158fddc19d5e095f0409c2914888d2
Ayushpandey01 <pandeyayush2511@gmail.com>
2024-10-24 11:10:46 +0530

    Add 2nd functionality to demo file

--- JUnit/src/main/java/org/example/demo.java
+++ JUnit/src/main/java/org/example/demo.java
@@ -1,5 +1,10 @@
 class Demo{
     public static void main(String[] args){
-        System.out.println("this is a demo file. ")
+        System.out.println("this is a demo file. ");
+        String str= "Looking for an entry-level position in a growing financial organization. Passionate about learning business concepts and possess the ability to handle work pressure and customer issues professionally. Have exceptional advisory skills to introduce new schemes and help customers to meet their financial goals.";
+        String[] words= str.split(" ");
+        for (String word:words){
+            System.out.println(word);
+        }
     }
 }
